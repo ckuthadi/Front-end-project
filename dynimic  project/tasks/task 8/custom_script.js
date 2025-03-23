2e108302ff7c8b500ce9e4e5c19da91f8688c758
ckuthadi <ckuthadi@aum.edu>
2025-03-22 22:57:00 -0500

    Add files via upload

---
+++ dynimic  project/tasks/task 8/custom_script.js
@@ -0,0 +1,6 @@
+function update(){
+    let txt = document.getElementById("input").value;
+    let output = txt.toUpperCase();
+    document.getElementById("output").value = output;
+    }
+    
---
+++ dynimic  project/tasks/task 9/script.js
@@ -0,0 +1,16 @@
+function update(){
+    let score = parseFloat(document.getElementById("input").value);
+    let grade = "";
+    if(score >= 80){
+    grade = "A";
+    }else if(score >=60 && score <=79){
+    grade = "B";
+    }else if(score>=40 && score <=59){
+    grade = "C";
+    }else if(score >=0 && score <=39){
+    grade = "F";
+    }
+    document.getElementById("grade_span").innerHTML = grade
+    }
+    console.log(grade);
+    
